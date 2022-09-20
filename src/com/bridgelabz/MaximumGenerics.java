e78425c24745c0f783e2d271b55c4cfd12e54147
Pranali-22 <lpranali22@gmail.com>
2022-09-21 00:29:51 +0530

    Added: UC2- Maximum among 3 float

--- src/com/bridgelabz/MaximumGenerics.java
+++ src/com/bridgelabz/MaximumGenerics.java
@@ -14,41 +14,37 @@ public class MaximumGenerics {
 	 */
 	
 	//Find maximum among 3 integers
-	public static void maximumof3Integer(Integer num1, Integer num2, Integer num3) {
-		Integer max = num1;
-		if(num2.compareTo(max) > 0) {
-			max=num2;
+	public static void maximumof3(Integer[] integerNumberArray) {
+		Integer max = integerNumberArray[0];
+		for(Integer e : integerNumberArray) {
+			if(e.compareTo(max) > 0) {
+				max=e;
+			}
 		}
 		
-		if(num3.compareTo(max) > 0) {
-			max=num3;
-		}
-		
-		System.out.println("Maximum of integer = "+max);
+		System.out.println("Maximum Integer = "+max);
 	}
 	
 	//Find maximum among 3 float
-	public static void maximumof3Float(Float num1, Float num2, Float num3) {
-		Float max = num1;
-		if(num2.compareTo(max) > 0) {
-			max=num2;
-		}
+	public static void maximumof3(Float[] integerFloatArray) {
 		
-		if(num3.compareTo(max) > 0) {
-			max=num3;
+		Float max = integerFloatArray[0];
+		for(Float e : integerFloatArray) {
+			if(e.compareTo(max) > 0) {
+				max=e;
+			}
 		}
-		
 		System.out.println("Maximum of float = "+max);
 	}
 	
 	public static void main(String[] args) {
 		//maximum for integer numbers
-		Integer integerNum1=10, integerNum2=25, integerNum3=16;
-		MaximumGenerics.maximumof3Integer(integerNum1,integerNum1,integerNum1);
+		Integer[] integerNumber = {10,25,17};
+		MaximumGenerics.maximumof3(integerNumber);
 		
 		//maximum for float numbers
-		Float floatNum1 = (float) 1.1 , floatNum2 = (float) 2.2, floatNum3 = (float) 3.3; 
-		MaximumGenerics.maximumof3Float(floatNum1, floatNum2, floatNum3);
+		Float[] floatNumber = {(float) 1.1, (float) 2.2, (float) 3.3} ;
+		MaximumGenerics.maximumof3(floatNumber);
 		
 	}
 

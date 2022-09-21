903d298fd2201c84ef76eaf494d0532f51be4b24
Pranali-22 <lpranali22@gmail.com>
2022-09-21 18:17:37 +0530

    Added: UC5 - Print max object using generic method

--- src/com/bridgelabz/MaximumGenerics.java
+++ src/com/bridgelabz/MaximumGenerics.java
@@ -22,20 +22,27 @@ public class MaximumGenerics {
 			}
 			return max;
 		}
+		
+		//Create generic method printMaximum to print maximum object
+		public static<T> void printMaximum(T maxInput) {
+			System.out.println("Maximum = "+maxInput);
+		}
 					
 		public static void main(String[] args) {
 			//maximum for integer numbers
 			Integer integerNum1 = 10, integerNum2=25, integerNum3=17, integerNum4=60;
-			System.out.println("Maximum of Integer = "+maximumof3(integerNum1, integerNum2, integerNum3, integerNum4));
+			Integer maxInt = maximumof3(integerNum1, integerNum2, integerNum3, integerNum4);
+			printMaximum(maxInt);
 			
 			//maximum for float numbers
 			Float floatNum1 = (float) 1.1, floatNum2 = (float) 2.2, floatNum3 = (float) 3.3;
-			System.out.println("Maximum of Float = "+maximumof3(floatNum1, floatNum2, floatNum3));
+			Float maxFloat = maximumof3(floatNum1, floatNum2, floatNum3);
+			printMaximum(maxFloat);
 			
 			//maximum for String
 			String stringValue1 = "Pranali", stringValue2 = "Rutuja";
-			System.out.println("Maximum of String = "+maximumof3(stringValue1, stringValue2));
-			
+			String maxString = maximumof3(stringValue1, stringValue2);
+			printMaximum(maxString);
 		}
 
 }

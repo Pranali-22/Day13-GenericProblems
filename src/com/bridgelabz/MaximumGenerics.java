addaa9ba2dae033a17b17a0a6c7668b8b054a5e0
Pranali-22 <lpranali22@gmail.com>
2022-09-21 18:08:21 +0530

    Added: UC4 - Extend the maximum method to take more than three

--- src/com/bridgelabz/MaximumGenerics.java
+++ src/com/bridgelabz/MaximumGenerics.java
@@ -13,30 +13,28 @@ public class MaximumGenerics {
 	 * @param args
 	 */
 	
-	//Find maximum among 3 integers
-		public static <E extends Comparable<E>> E maximumof3(E input1, E input2, E input3) {
-			E max = input1;
-			if(input2.compareTo(max) > 0) {
-					max=input2;
-			}
-			if(input3.compareTo(max) > 0) {
-				max=input3;
+	//Find maximum among n number of inputs
+		public static <T extends Comparable<T>> T maximumof3(T... inputs) {
+			T max = inputs[0];
+			for(T input : inputs)
+			if(input.compareTo(max) > 0) {
+				max=input;
 			}
 			return max;
 		}
 					
 		public static void main(String[] args) {
 			//maximum for integer numbers
-			Integer integerNum1 = 10, integerNum2=25, integerNum3=17;
-			System.out.println("Maximum of Integer = "+maximumof3(integerNum1, integerNum2, integerNum3));
+			Integer integerNum1 = 10, integerNum2=25, integerNum3=17, integerNum4=60;
+			System.out.println("Maximum of Integer = "+maximumof3(integerNum1, integerNum2, integerNum3, integerNum4));
 			
 			//maximum for float numbers
 			Float floatNum1 = (float) 1.1, floatNum2 = (float) 2.2, floatNum3 = (float) 3.3;
 			System.out.println("Maximum of Float = "+maximumof3(floatNum1, floatNum2, floatNum3));
 			
 			//maximum for String
-			String stringValue1 = "Pranali",stringValue2 = "Tani", stringValue3 = "Rutuja";
-			System.out.println("Maximum of String = "+maximumof3(stringValue1, stringValue2, stringValue3));
+			String stringValue1 = "Pranali", stringValue2 = "Rutuja";
+			System.out.println("Maximum of String = "+maximumof3(stringValue1, stringValue2));
 			
 		}
 
